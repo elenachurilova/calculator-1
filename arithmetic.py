052e0044ddeca53273eb97f66ee28d351d27ea0f
Elena Churilova <ellena.churilova@gmail.com>
2020-07-08 13:18:38 -0700

    Multiply function

--- arithmetic.py
+++ arithmetic.py
@@ -16,7 +16,7 @@ def subtract(num1, num2):
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
-    #return (num1 * num2)
+    return (num1 * num2)
 
 
 def divide(num1, num2):
